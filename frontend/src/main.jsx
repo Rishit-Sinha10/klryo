902685d2daacf8e64d9f6d3184feeea6d7ed725e
Ramesh1234-ai <Sinharishit04@gmail.com>
2026-09-16 14:38:28 +0530

    Adding sentry verification code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,22 +6,34 @@ import { ToastProvider } from "./context/ToastContext";
 import "./index.css";
 import App from "./App";
 import { Analytics } from "@vercel/analytics/react";
-
+import * as Sentry from "@sentry/react";
 const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk publishable key");
 }
-
+Sentry.init({
+  dsn: "https://2593269533fb692aeb69f875e01082fc@o4512095166988288.ingest.de.sentry.io/4512095174131792",
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  dataCollection: {
+    // userInfo: false,
+    // httpBodies: []
+  },
+});
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
-      <ThemeProvider>
-        <ToastProvider>
-          <Analytics />
-          <App />
-        </ToastProvider>
-      </ThemeProvider>
-    </ClerkProvider>
-  </BrowserRouter>
+  <Sentry.ErrorBoundary
+    fallback={<p>Something went wrong. We've been notified.</p>}
+  >
+    <BrowserRouter>
+      <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+        <ThemeProvider>
+          <ToastProvider>
+            <Analytics />
+            <App />
+          </ToastProvider>
+        </ThemeProvider>
+      </ClerkProvider>
+    </BrowserRouter>
+  </Sentry.ErrorBoundary>,
 );
